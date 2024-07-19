2b1395af2c567968618eec60cc37e9040bff9d17
Gustavo Nakamura <gustavonakamura@alunos.utfpr.edu.br>
2024-07-18 21:56:56 -0300

    Consts

--- src/PortfolioContainer/Resume/Resume.js
+++ src/PortfolioContainer/Resume/Resume.js
@@ -32,8 +32,47 @@ export default function Resume(props) {
   };
 
   const resumeBullets = [
-    {label: "Education", logoSrc: "education.svg"}
+    {label: "Education", logoSrc: "education.svg"},
+    {label: "Work History", logoSrc: "work-history.svg"},
+    {label: "Programming Skills", logoSrc: "programming-skills.svg"},
+    {label: "Projects", logoSrc: "projects.svg"},
+    {label: "Interests", logoSrc: "interests.svg"}
+  ];
+
+  const programmingSkillDetails = [
+    {skill: "Outsystems", ratingPercentage:85},
+    {skill: "React", ratingPercentage:85},
+    {skill: "Outsystems", ratingPercentage:85},
+    {skill: "Outsystems", ratingPercentage:85},
+    {skill: "Outsystems", ratingPercentage:85}
   ]
+
+  const projectsDetails = [
+    {
+      title: "Personal Portfolio Website",
+      duration: { fromDate: "2020", toDate: "2021" },
+      description:
+        "A Personal Portfolio website to showcase all my details and projects at one place.",
+      subHeading: "Technologies Used: React JS, Bootsrap",
+    },
+    {
+      title: "Mobile E-shop ",
+      duration: { fromDate: "2020", toDate: "2021" },
+      description:
+        "An ecommerce application designed to sell products online wth payment system integration",
+      subHeading:
+        "Technologies Used:  React Native, Mongo DB, Express Js, Node Js, Redux.",
+    },
+    {
+      title: "Ecommerce Website ",
+      duration: { fromDate: "2020", toDate: "2021" },
+      description:
+        "Online ecommerce website for showcasing and selling products onlne with payment system integration, both Paypal and Stripe",
+      subHeading:
+        "Technologies Used: Mongo DB, Epress Js, React Js, Node JS, Redux, Bootstrap.",
+    },
+  ];
+
 
   let fadeInScreenHandler = (screen) => {
     if (screen.FadeScreen !== props.id) return;
