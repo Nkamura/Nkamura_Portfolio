fbb8e2fc92e945c73719ce8ec4a7453c794d65d5
Gustavo Nakamura <gustavonakamura@alunos.utfpr.edu.br>
2024-08-21 17:53:38 -0300

    Projects

--- src/PortfolioContainer/Projects/Projects.js
+++ src/PortfolioContainer/Projects/Projects.js
@@ -1,15 +1,13 @@
-import React from 'react'
+import React from "react";
 import OwlCarousel from "react-owl-carousel";
 import "owl.carousel/dist/assets/owl.carousel.css";
 import "owl.carousel/dist/assets/owl.theme.default.css";
 
-import ScreenHeading from '../../utilities/ScreenHeading/ScreenHeading';
-import ScrollService from '../../utilities/ScrollService';
-import Animations from '../../utilities/Animations';
-
+import ScreenHeading from "../../utilities/ScreenHeading/ScreenHeading";
+import ScrollService from "../../utilities/ScrollService";
+import Animations from "../../utilities/Animations";
 
 export default function Projects(props) {
-
   let fadeInScreenHandler = (screen) => {
     if (screen.FadeScreen !== props.id) return;
     Animations.animations.fadeInScreen(props.id);
@@ -20,19 +18,25 @@ export default function Projects(props) {
 
   return (
     <div>
-      <ScreenHeading
-        title={'Projects'}
-        subHeading={'My projects'}
-      />
-      <section className='projects-section' id={props.id || ''}>
-        <div className='container'>
-          <div className='row'>
-            <OwlCarousel className='owl-carousel' id='projects-carousel'>
-              <div className='col-lg-12'>
-                <div className='project-item'>
-                  <div className='project-comments'>
+      <ScreenHeading title={"Projects"} subHeading={"My projects"} />
+      <section className="projects-section" id={props.id || ""}>
+        <div className="container">
+          <div className="row">
+            <OwlCarousel className="owl-carousel" id="projects-carousel">
+              <div className="col-lg-12">
+                <div className="project-item">
+                  <div className="project-comments">
                     <p>
-                      This project as develop for the colleger project.
+                      <i className="fa fa-quote-left" />
+                      College Project.
+                      <i className="fa fa-quote-right" />
+                    </p>
+                  </div>
+                  <div className="project-info">
+                    <img src="/public/png/fire.png" alt="python-picture"></img>
+                    <h5>Project name</h5>
+                    <p>
+                      Language: Python
                     </p>
                   </div>
                 </div>
@@ -42,5 +46,5 @@ export default function Projects(props) {
         </div>
       </section>
     </div>
-  )
+  );
 }
